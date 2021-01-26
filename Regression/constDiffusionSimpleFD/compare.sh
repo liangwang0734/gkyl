0aad98e9013cd53745f43dad44cb8321a4b3f05e
Liang Wang <liang.wang.phys@gmail.com>
2021-01-26 16:35:59 -0500

    Regression/constDiffusionSimpleFD/compare.sh consolidate

--- Regression/constDiffusionSimpleFD/compare.sh
+++ Regression/constDiffusionSimpleFD/compare.sh
@@ -1,5 +1,6 @@
 
+pgkyl -f 'rt-testConstDiffusionSimple-1d_q*_2.bp' plot -f0
 
-# pgkyl -f 'rt-testConstDiffusionSimple-2d_q*_2.bp' select --z0 0 plot -f0
+pgkyl -f 'rt-testConstDiffusionSimple-2d_q*_2.bp' select --z0 0 plot -f0
 
-# pgkyl -f 'rt-testConstDiffusionSimple-3d_q*_2.bp' select --z0 16 select --z1 16 plot -f0
+pgkyl -f 'rt-testConstDiffusionSimple-3d_q*_2.bp' select --z0 16 select --z1 16 plot -f0
